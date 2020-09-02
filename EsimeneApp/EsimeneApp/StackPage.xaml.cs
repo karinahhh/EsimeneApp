f4a88e3a7279c690ae2fa2982feac790f845c5f1
opilane <opilane@HP-CZC6297B8Z>
2020-09-02 15:43:07 +0300

    Add project files.

---
+++ EsimeneApp/EsimeneApp/StackPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EsimeneApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackPage : ContentPage
+    {
+        public StackPage()
+        {
+            InitializeComponent();
+        }
+        int clik = 0;
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            clik++;
+            btn.Text = clik.ToString();
+            if (clik % 5 == 0)
+            {
+                //lbl.IsVisible = false;
+                lbl.Text = "";
+            }
+            else
+            {
+                //lbl.IsVisible = true;
+                lbl.Text = "Nupp";
+            }
+        }
+
+        private void btn2_Clicked(object sender, EventArgs e)
+        {
+            clik = 0;
+            btn.Text = clik.ToString();
+        }
+    }
+}
